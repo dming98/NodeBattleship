15e77f35e96241e108d290922d0776177d05b4de
Edwin <116839874+EdwinCloude@users.noreply.github.com>
2023-07-11 02:03:37 -0400

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,13 @@
 const { defineConfig } = require("cypress");
 
+require("cypress-mochawesome-reporter/plugin");
+
 module.exports = defineConfig({
   projectId: 'nzhz2a',
   e2e: {
     defaultBrowser: 'chrome',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      // Add Mochawesome reporter
-      require("cypress-mochawesome-reporter/plugin")(on, config, {
-        reportDir: "cypress/results",
-        overwrite: false,
-        html: false,
-        json: true
-      });
-
-      return config;
     },
   },
 });
